0928653a99f3220e826d74d0d5877bca8df57626
David Lawton <davidj.lawton@gmail.com>
2024-10-22 15:25:34 +0100

    Finished CompSim Assignment 3

--- Junior Sophister/Computer Simulation/Numerical Methods/Assignment 3/CompSimAssignment3_DavidLawton.py
+++ Junior Sophister/Computer Simulation/Numerical Methods/Assignment 3/CompSimAssignment3_DavidLawton.py
@@ -20,9 +20,12 @@ plt.close()
 
 # Parameters
 t = 0
-dt = 0.04 
+dt_vals = [0.04, 0.02] 
 x_0 = 0.0655
-t_f_vals = [1.5, 2.5, 4]
+t_f_vals = [2.5, 4]
+
+# Indexing for the subplots
+title_indexing = ['(a)', '(b)', '(c)']
 
 
 #Simple Euler Method
@@ -40,22 +43,24 @@ def e_method(x_initial, t_final, dt):
 
 
 # Plot the simple Euler method
-fig, ax = plt.subplots(1,3)
-fig.set_size_inches(12, 6)
-fig.suptitle("Euler Method Approximation")
+for dt in dt_vals:
+    fig, ax = plt.subplots(1,2)
+    fig.set_size_inches(14, 6)
+    fig.suptitle(f"Euler Method Approximation, $\Delta t = {dt}s$")
 
-for i in range(len(t_f_vals)):
-    x_vals, t_vals = e_method(x_0, t_f_vals[i], dt)
-    grid_points = np.meshgrid(np.linspace(0, t_f_vals[i], int(len(t_vals)/5)), np.linspace(np.min(x_vals), np.max(x_vals), int(len(x_vals)/5)))
-    ax[i].plot(t_vals, x_vals)
-    ax[i].quiver(grid_points[0], grid_points[1], 1, f(grid_points[1], grid_points[0]), scale=50*np.max(x_vals))
-    ax[i].set_title(f"$t_f = {t_f_vals[i]}s$")
-    ax[i].set_xlabel("$t~(s)$")
-    ax[i].set_ylabel("$x~(m)$")
+    for i in range(len(t_f_vals)):
+        x_vals, t_vals = e_method(x_0, t_f_vals[i], dt)
+        grid_points = np.meshgrid(np.linspace(0, t_f_vals[i], int(len(t_vals)/5)), np.linspace(np.min(x_vals), np.max(x_vals), int(len(x_vals)/5)))
+        ax[i].plot(t_vals, x_vals)
+        ax[i].quiver(grid_points[0], grid_points[1], 1, f(grid_points[1], grid_points[0]), scale=50*np.max(x_vals))
+        ax[i].set_title(f"{title_indexing[i]} $t_f = {t_f_vals[i]}s$")
+        ax[i].set_xlabel("$t~(s)$")
+        ax[i].set_ylabel("$x~(m)$")
 
-plt.savefig("/home/dj-lawton/Documents/Junior Sophister/Computer Simulation/Numerical Methods/Assignment 3/Euler_Method_Approximation_0.04.pdf")
-plt.show()
-plt.close()
+    plt.savefig(f"/home/dj-lawton/Documents/Junior Sophister/Computer Simulation/Numerical Methods/Assignment 3/Euler_Method_Approximation_{dt}.pdf")
+    plt.show()
+    plt.close()
+
 
 
 # Improved Euler method
@@ -91,37 +96,38 @@ def Runge_Kutta_4th_Order(x_initial, t_final, dt):
 
 
 # Plotting the higher order approximations
-fig, ax = plt.subplots(1,3)
-fig.set_size_inches(18, 6)  
-fig.suptitle("Higher Order Approximations")
+for dt in dt_vals:
+    fig, ax = plt.subplots(1,2)
+    fig.set_size_inches(14, 6)  
+    fig.suptitle(f"Higher Order Approximations, $\Delta t = {dt}s$")
 
-for i in range(len(t_f_vals)):
-    
-    # Calling the methods
-    e_method_x_vals, e_method_t_vals = e_method(x_0, t_f_vals[i], dt)
-    improved_e_method_x_vals, improved_e_method_t_vals = improved_euler_method(x_0, t_f_vals[i], dt)
-    runge_kutta_method_x_vals, runge_kutta_method_t_vals = Runge_Kutta_4th_Order(x_0, t_f_vals[i], dt)
+    for i in range(len(t_f_vals)):
 
-    # Generating the direction field
-    grid_points = np.meshgrid(np.linspace(0, t_f_vals[i], int(len(e_method_t_vals)/6)), np.linspace(np.min(runge_kutta_method_x_vals), np.max(e_method_x_vals), int(len(e_method_x_vals)/6)))
+        # Calling the methods
+        e_method_x_vals, e_method_t_vals = e_method(x_0, t_f_vals[i], dt)
+        improved_e_method_x_vals, improved_e_method_t_vals = improved_euler_method(x_0, t_f_vals[i], dt)
+        runge_kutta_method_x_vals, runge_kutta_method_t_vals = Runge_Kutta_4th_Order(x_0, t_f_vals[i], dt)
+
+        # Generating the direction field
+        grid_points = np.meshgrid(np.linspace(0, t_f_vals[i], int(len(e_method_t_vals)/6)), np.linspace(np.min(runge_kutta_method_x_vals), np.max(e_method_x_vals), int(len(e_method_x_vals)/6)))
 
 
-    ax[i].set_title(f"$t_f = {t_f_vals[i]}s$")
+        ax[i].set_title(f"{title_indexing[i]} $t_f = {t_f_vals[i]}s$")
 
-    ax[i].quiver(grid_points[0], grid_points[1], 1, f(grid_points[1], grid_points[0]), scale=40*np.max(e_method_x_vals))
+        ax[i].quiver(grid_points[0], grid_points[1], 1, f(grid_points[1], grid_points[0]), scale=40*np.max(e_method_x_vals))
 
-    ax[i].plot(e_method_t_vals, e_method_x_vals, label='Euler Method')
-    ax[i].plot(improved_e_method_t_vals, improved_e_method_x_vals, label='Improved Euler Method')
-    ax[i].plot(runge_kutta_method_t_vals, runge_kutta_method_x_vals, label='Runge-Kutta 4th Order')
+        ax[i].plot(e_method_t_vals, e_method_x_vals, label='Euler Method')
+        ax[i].plot(improved_e_method_t_vals, improved_e_method_x_vals, label='Improved Euler Method')
+        ax[i].plot(runge_kutta_method_t_vals, runge_kutta_method_x_vals, label='Runge-Kutta 4th Order')
 
-    ax[i].set_xlabel("$t~(s)$")
-    ax[i].set_ylabel("$x~(m)$")
+        ax[i].set_xlabel("$t~(s)$")
+        ax[i].set_ylabel("$x~(m)$")
 
-    ax[i].legend()
+        ax[i].legend()
 
-plt.savefig("/home/dj-lawton/Documents/Junior Sophister/Computer Simulation/Numerical Methods/Assignment 3/Higher_Order_Approximations_0.04.pdf")
-plt.show()
-plt.close()
+    plt.savefig(f"/home/dj-lawton/Documents/Junior Sophister/Computer Simulation/Numerical Methods/Assignment 3/Higher_Order_Approximations_{dt}.pdf")
+    plt.show()
+    plt.close()
 
 print('Name: David Lawton', '\n', 'Student No.: 22337087')
 
